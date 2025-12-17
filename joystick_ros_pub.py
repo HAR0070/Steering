8098956bbfcef47b3acf1ab0539f651f1efd1af6
HAR <64668639+HAR0070@users.noreply.github.com>
2025-12-18 00:06:02 +0530

    Added position PID loop

--- joystick_ros_pub.py
+++ joystick_ros_pub.py
@@ -14,6 +14,12 @@ BAUD_RATE = 9600
 DEADZONE = 0.1
 SEND_INTERVAL = 0.05 
 
+KP = 10
+KD = 0.5
+KI = 0.5
+SPD_REF = 4000
+CUR_LIM = 2.5
+
 def find_controller():
     """Initializes pygame and finds the first available joystick."""
     print("Initializing controller...")
@@ -40,7 +46,9 @@ def to_twist(x , y=None):
     return msg
 
 def to_msg(line , y=None):
+
     msg = Float32MultiArray()
+
     content = line.strip().split(" ")
     check = set(content)
     if "RX" in check:
@@ -51,15 +59,36 @@ def to_msg(line , y=None):
         err = float(content[-1])
         msg.data = [position, speed, current , temperature, err]
     else:
-        msg.data = [position, speed, current , temperature, 100]
+        msg.data = [0, 0, 0 , 0, 100]
 
     return msg
 
-def steering_pub(msg):
-    
-    while not rospy.is_shutdown():
-        pub.publish(msg)
-        rate.sleep()
+def pid_pos_vel(ref_pos , pos_fb , speed_fb , curr_fb , integral_err):
+
+    pos_err = ref_pos - pos_fb          # if ref > current - velocity -ve
+    spd_err = -speed_fb     
+
+    if abs(pos_err) < 1.5:
+        integral_err = 0
+    else:
+        integral_err += pos_err
+
+    velocity = KP * pos_err + KD * spd_err + KI*integral_err ## positive position error -> Positive velocity 
+
+    if velocity > SPD_REF:
+        velocity = SPD_REF
+        integral_err -= pos_err
+    elif -velocity > SPD_REF:
+        velocity = -SPD_REF
+        integral_err -= pos_err
+        
+    if curr_fb > CUR_LIM:
+        print(f"your hitting current limit {curr_fb}")
+        velocity *=0.5
+
+    print(f" vel = {velocity} pos_err = {pos_err} spd_err = {spd_err} pos_fb = {pos_fb}  pos_ref = {ref_pos} spd_fb = {speed_fb}  \r")
+
+    return velocity , integral_err
 
 def connect_to_arduino(port, baud):
     """Tries to connect to the Arduino over serial."""
@@ -74,19 +103,19 @@ def connect_to_arduino(port, baud):
         print(f"Error: Could not open serial port {port}.")
         return None
 
-def map_axis_to_velocity(axis_value, inverted=False):
+def map_axis_to_position(axis_value, inverted=False):
     """Converts a joystick axis (-1.0 to 1.0) to a velocity (-100 to 100)."""
     # Apply deadzone
     if abs(axis_value) < DEADZONE:
         axis_value = 0.0
         
-    # Map the value from [-1.0, 1.0] to [-1000, 1000]
-    velocity = int(axis_value * 2500)
+    # Map the value from [-1.0, 1.0] to [-520, 520]
+    position = int(axis_value * 3100)
 
     if inverted:
-        velocity = -velocity
+        position = -position
         
-    return velocity
+    return position
 
 def main_loop( controller, arduino_serial , filename=None):
     """The main application loop."""
@@ -97,29 +126,42 @@ def main_loop( controller, arduino_serial , filename=None):
         rospy.init_node('steering_pub')
         rate = rospy.Rate(20) # 20Hz
 
+        integral_err = 0
 
-        while True:
+        while not rospy.is_shutdown():
             # This is CRITICAL. You must pump the event queue.
             pygame.event.pump()
 
             axis_x = controller.get_axis(0)
-            axis_y = controller.get_axis(1)
+            # axis_y = controller.get_axis(1)
 
-            vel_x = map_axis_to_velocity(axis_x)
-            vel_y = map_axis_to_velocity(axis_y, inverted=True)
+            pos_x = map_axis_to_position(axis_x)
+            # pos_y = map_axis_to_position(axis_y, inverted=True)
 
-            message = f"<{vel_x},{vel_y}>\n"
-            
-            # Send the message as bytes
             try:
-                # start_time = time.time()
-                arduino_serial.write(message.encode('utf-8'))
-                # end_time = time.time() # Record end time
-                # elapsed_time = end_time - start_time
-                # print(f"Sent: X={vel_x} Y={vel_y}  (Raw: {message.strip()})  elapsed_time : {elapsed_time}", end='\r')
-                msg = to_twist(vel_x)
+                if arduino_serial.in_waiting > 0:
+                    # Read all available bytes and throw them away (or print them for debug)
+                    line = arduino_serial.readline().decode('utf-8', errors='ignore').strip()
+                    if line:  
+                        msg = to_msg(line)
+                        pos_fb = msg.data[0]
+                        speed_fb = msg.data[1]
+                        curr_fb = msg.data[2]
+                        pub_motor_fb.publish(msg)
+                        if msg.data[-1] != 100:
+                            vel_x , integral_err = pid_pos_vel(pos_x , pos_fb , speed_fb, curr_fb , integral_err)
+                            # print(f"vel_x = {vel_x}  pos_ref = {pos_x} pos_fb = {pos_fb}")
+            
+                            message = f"<{vel_x},{pos_x}>\n"
 
-                pub_steering.publish(msg)
+                            # start_time = time.time()
+                            arduino_serial.write(message.encode('utf-8'))
+                            msg = to_twist(vel_x)
+
+                            pub_steering.publish(msg)
+            except IndexError:
+                pass
+            
             except serial.SerialException as e:
                 print(f"\n Serial write error: {e}")
                 raise
@@ -131,50 +173,35 @@ def main_loop( controller, arduino_serial , filename=None):
             if filename:
                 with open(filename, "a") as log_file:
                     try:
-                        if arduino_serial.in_waiting > 0:
-                            # Read all available bytes and throw them away (or print them for debug)
-                            line = arduino_serial.readline().decode('utf-8', errors='ignore').strip()
-                            if line:
-                                    # Get timestamp
-                                    timestamp = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                                    
-                                    # Format log entry
-                                    log_entry = f"[{timestamp}] Cmd: <{vel_x},{vel_y}> | Fb: {line}"
-                                    
-                                    # 1. Print to Console (One line)
-                                    print(f"{log_entry}          ", end='\n', flush=True)
-                                    
-                                    # 2. Write to File
-                                    log_file.write(log_entry + "\n")
-                                    # Force write to disk immediately (safer if script crashes)
-                                    log_file.flush() 
+                        if line:
+                                # Get timestamp
+                                timestamp = datetime.datetime.now().strftimvel_xe("%H:%M:%S.%f")[:-3]
+                                
+                                log_entry = f"[{timestamp}] Cmd: <{pos_x},{vel_x}> | Fb: {line}"
+                                
+                                # 1. Print to Console (One line)
+                                print(f"{log_entry}          ", end='\n', flush=True)
+                                
+                                # 2. Write to File
+                                log_file.write(log_entry + "\n")
+                                # Force write to disk immediately (safer if script crashes)
+                                log_file.flush() 
+                    except KeyboardInterrupt:
+                        raise
                     except Exception as e:
                         pass
-            else:
-                try:
-                    if arduino_serial.in_waiting > 0:
-                        # Read all available bytes and throw them away (or print them for debug)
-                        line = arduino_serial.readline().decode('utf-8', errors='ignore').strip()
-                        if line:  
-                            msg = to_msg(line)
-                            pub_motor_fb.publish(msg)
-                              
-                except Exception as e:
-                    pass
-                
+
             rate.sleep()  
-            # Don't spam the Arduino, send at a fixed rate
-            # time.sleep(SEND_INTERVAL)
+
     except Exception as e:
         print(f"the main loop broke due to error {e}")
 
     except KeyboardInterrupt:
         print("\nExiting program.")
+        sys.exit(1)
     finally:
         # Clean up
         if arduino_serial and arduino_serial.is_open:
-            # Send a "stop" command before closing
-            # arduino_serial.write(b"<0,0>\n")
             arduino_serial.close()
         pygame.joystick.quit()
         pygame.quit()
